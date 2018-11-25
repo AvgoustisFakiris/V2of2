a37ea3f6ba0f441878e9c8a55ca93c1d6f052c58
AvgoustisFakiris <avgfakiris@gmail.com>
2018-11-25 17:01:21 +0200

    Privacy Policy

--- app/src/main/java/gr/hua/www/v2of2/BaseActivity.java
+++ app/src/main/java/gr/hua/www/v2of2/BaseActivity.java
@@ -12,6 +12,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
@@ -291,6 +292,112 @@ public class BaseActivity extends AppCompatActivity implements
     public void setMNC(String mnc) {
         BaseActivity.mnc = mnc;
     }
+    //Privacy Policy Message
+    private String myvar = "Effective date: October 12, 2018\n" +
+            "\n" +
+            "V2OF . This SERVICE is provided by Dr. Vassilis Dalakas - Avgoustis Fakiris as a Diploma Project at no cost and is intended for use as is. \n" +
+            "\n" +
+            "This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. Our Privacy Policy for V2OF is managed through Free Privacy Policy.\n" +
+            "\n" +
+            "We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions.\n" +
+            "\n" +
+            "Information Collection And Use\n" +
+            "We collect several different types of information for various purposes to provide and improve our Service to you.\n" +
+            "\n" +
+            "Types of Data Collected\n" +
+            "Personal Data\n" +
+            "While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you (\"Personal Data\"). Personally identifiable information may include, but is not limited to:\n" +
+            "\n" +
+            "Email address\n" +
+            "Address, State, Province, ZIP/Postal code, City\n" +
+            "Cookies and Usage Data\n" +
+            "Usage Data\n" +
+            "We may also collect information that your browser sends whenever you visit our Service or when you access the Service by or through a mobile device (\"Usage Data\").\n" +
+            "\n" +
+            "This Usage Data may include information such as your computer's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.\n" +
+            "\n" +
+            "When you access the Service by or through a mobile device, this Usage Data may include information such as the type of mobile device you use, your mobile device unique ID, the IP address of your mobile device, your mobile operating system, the type of mobile Internet browser you use, unique device identifiers and other diagnostic data.\n" +
+            "\n" +
+            "Tracking & Cookies Data\n" +
+            "We use cookies and similar tracking technologies to track the activity on our Service and hold certain information.\n" +
+            "\n" +
+            "Cookies are files with small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.\n" +
+            "\n" +
+            "You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.\n" +
+            "\n" +
+            "Examples of Cookies we use:\n" +
+            "\n" +
+            "Session Cookies. We use Session Cookies to operate our Service.\n" +
+            "Preference Cookies. We use Preference Cookies to remember your preferences and various settings.\n" +
+            "Security Cookies. We use Security Cookies for security purposes.\n" +
+            "Use of Data\n" +
+            "V2OF uses the collected data for various purposes:\n" +
+            "\n" +
+            "To provide and maintain the Service\n" +
+            "To notify you about changes to our Service\n" +
+            "To allow you to participate in interactive features of our Service when you choose to do so\n" +
+            "To provide customer care and support\n" +
+            "To provide analysis or valuable information so that we can improve the Service\n" +
+            "To monitor the usage of the Service\n" +
+            "To detect, prevent and address technical issues\n" +
+            "Transfer Of Data\n" +
+            "Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.\n" +
+            "\n" +
+            "If you are located outside Greece and choose to provide information to us, please note that we transfer the data, including Personal Data, to Greece and process it there.\n" +
+            "\n" +
+            "Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.\n" +
+            "\n" +
+            "V2OF will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.\n" +
+            "\n" +
+            "Disclosure Of Data\n" +
+            "Legal Requirements\n" +
+            "V2OF may disclose your Personal Data in the good faith belief that such action is necessary to:\n" +
+            "\n" +
+            "To comply with a legal obligation\n" +
+            "To protect and defend the rights or property of V2OF\n" +
+            "To prevent or investigate possible wrongdoing in connection with the Service\n" +
+            "To protect the personal safety of users of the Service or the public\n" +
+            "To protect against legal liability\n" +
+            "Security Of Data\n" +
+            "The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.\n" +
+            "\n" +
+            "Service Providers\n" +
+            "We may employ third party companies and individuals to facilitate our Service (\"Service Providers\"), to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.\n" +
+            "\n" +
+            "These third parties have access to your Personal Data only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.\n" +
+            "\n" +
+            "Analytics\n" +
+            "We may use third-party Service Providers to monitor and analyze the use of our Service.\n" +
+            "\n" +
+            "Firebase\n" +
+            "\n" +
+            "Firebase is analytics service provided by Google Inc.\n" +
+            "\n" +
+            "You may opt-out of certain Firebase features through your mobile device settings, such as your device advertising settings or by following the instructions provided by Google in their Privacy Policy: https://policies.google.com/privacy?hl=en\n" +
+            "\n" +
+            "We also encourage you to review the Google's policy for safeguarding your data: https://support.google.com/analytics/answer/6004245. For more information on what type of information Firebase collects, please visit please visit the Google Privacy & Terms web page: https://policies.google.com/privacy?hl=en\n" +
+            "\n" +
+            "Links To Other Sites\n" +
+            "Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.\n" +
+            "\n" +
+            "We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.\n" +
+            "\n" +
+            "Children's Privacy\n" +
+            "Our Service does not address anyone under the age of 18 (\"Children\").\n" +
+            "\n" +
+            "We do not knowingly collect personally identifiable information from anyone under the age of 18. If you are a parent or guardian and you are aware that your Children has provided us with Personal Data, please contact us. If we become aware that we have collected Personal Data from children without verification of parental consent, we take steps to remove that information from our servers.\n" +
+            "\n" +
+            "Changes To This Privacy Policy\n" +
+            "We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.\n" +
+            "\n" +
+            "We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the \"effective date\" at the top of this Privacy Policy.\n" +
+            "\n" +
+            "You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.\n" +
+            "\n" +
+            "Contact Us\n" +
+            "If you have any questions about this Privacy Policy, please contact us:\n" +
+            "\n" +
+            "By email: vdalakas@hua.gr , avg.fakiris@gmail.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -412,6 +519,10 @@ public class BaseActivity extends AppCompatActivity implements
                 VersionHelper.refreshActionBarMenu(this);
                 Intent intent = new Intent(getApplicationContext(), MapView.class);
                 startActivity(intent);
+                return true;
+            case R.id.menu_privacy:
+                VersionHelper.refreshActionBarMenu(this);
+                showPrivacyDialog();
                 return true;
 
         }
@@ -1018,5 +1129,24 @@ public class BaseActivity extends AppCompatActivity implements
         }
         Log.d(TAG, new Object() {
         }.getClass().getEnclosingMethod().getName());
+    }
+
+    //Show Privacy Policy Message
+    private void showPrivacyDialog() {
+        new android.app.AlertDialog.Builder(this)
+                .setTitle("Privacy Policy of V2of")
+                .setIcon(R.mipmap.ic_launcher)
+                .setMessage(myvar)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create().show();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.apply();
     }
 }
